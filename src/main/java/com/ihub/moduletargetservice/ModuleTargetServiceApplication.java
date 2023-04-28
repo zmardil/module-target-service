8b718e0f7d8bb81d386637220b1ed715b12ce9e5
zmardil <btzardil@gmail.com>
2023-04-28 18:34:13 +0530

    Project setup

---
+++ src/main/java/com/ihub/moduletargetservice/ModuleTargetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ihub.moduletargetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleTargetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleTargetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ihub/moduletargetservice/ModuleTargetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihub.moduletargetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleTargetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
