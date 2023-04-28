e2a970bef61787ee9b0af27c8fe65f5f47717962
zmardil <btzardil@gmail.com>
2023-04-28 21:40:34 +0530

    Change main() class to App

--- src/main/java/com/ihub/moduletargetservice/App.java
+++ src/main/java/com/ihub/moduletargetservice/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModuleTargetServiceApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModuleTargetServiceApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
